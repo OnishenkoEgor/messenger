53046338eaa29dd3cafb1ce662b6141b6e6bd007
OnishenkoEgor <egoronishenko@yandex.ru>
2024-05-12 00:29:55 +0300

    added front

--- entrypoints/entrypoint.sh
+++ entrypoints/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 composer install
-
-#php artisan migrate  --force --seed
+php artisan key:generate
+php artisan migrate  --force --seed
 
 # Run the main container command
 php-fpm
